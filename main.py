622a920a3b4553bf51ee762c205b71c8d32c79e7
kindiregg <jpriveraa7@gmail.com>
2025-02-08 22:16:09 -0600

    Turned audio into numpy arrays by chunks of 1024

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
-import wave
 import numpy as np
 
-from process_audio import process_audio
+from process_audio import get_audio_data, get_audio_chunks
 CHUNK = 1024
 
-audio_path = "audio/exampleaudio.wav"
+audio_path = "audio/Eastghost - Sunshine (Kindrid Bootleg) [16bit].wav"
+audio_metadata = {}
 
-file_paramaters = tuple()
 def main():
-    process_audio(audio_path)
-
+    print("Reading wav file...")
+    get_audio_data(audio_path, audio_metadata)
+    audio_chunks = get_audio_chunks(audio_path, CHUNK, audio_metadata)
+    # print(f"Audio chunks: {audio_chunks}")
+                                        
 main()
--- process_audio.py
+++ process_audio.py
@@ -1,27 +1,79 @@
 import wave
 import numpy as np
 
-def process_audio(audio_path):
-    print("Reading wav file...")
-    song_file = wave.open(audio_path, "rb")
-    # print(song_file.readframes(10)) 
-    nchannels = song_file.getnchannels()
-    print(f"Number of channels: {nchannels}")
-    bwitdh = song_file.getsampwidth()
-    print(f"Sample width: {bwitdh}")
-    bdepth = song_file.getsampwidth() * 8
-    print(f"Bit depth: {bdepth}")
-    comp_type = song_file.getcomptype()
-    print(f"Compression type: {comp_type}")
-    comp_name = song_file.getcompname()
-    print(f"Compression name: {comp_name}")
-    sample_rate = song_file.getframerate()
-    print(f"Sample rate: {sample_rate}")
-    length = song_file.getnframes()
-    length_time = length / sample_rate / 60
-    length_mins = int(length_time)
-    length_seconds_remainder = int((length_time - length_mins) * 60)
-    print(f"Length: {length_mins} minutes {length_seconds_remainder} seconds")
-    song_file.close()
+def print_audio_metadata(audio_metadata):
+    # For debugging mostly
+    print(f"Number of channels: {audio_metadata['num_channels']}")
+    print(f"Bit depth: {audio_metadata['bit_depth']}")
+    print(f"Compression type: {audio_metadata['comp_type']}")
+    print(f"Compression name: {audio_metadata['comp_name']}")
+    print(f"Sample rate: {audio_metadata['sample_rate']}")
+    print(f"Sample length: {audio_metadata['length_mins']} minutes and {audio_metadata['length_seconds_remainder']} seconds")
 
-# TODO: numpy array of audio data
+def get_audio_data(audio_path, audio_metadata):
+    song_file = wave.open(audio_path, "rb")
+    # Placing all relevant paramaters into a dictionary
+    audio_metadata["num_channels"] = song_file.getnchannels()
+    audio_metadata["sample_width"] = song_file.getsampwidth()
+    audio_metadata["bit_depth"] = audio_metadata["sample_width"] * 8
+    audio_metadata["comp_type"] = song_file.getcomptype()
+    audio_metadata["comp_name"] = song_file.getcompname()
+    audio_metadata["sample_rate"] = song_file.getframerate()
+    audio_metadata["lengh_samples"] = song_file.getnframes()
+    audio_metadata["length_time"] = audio_metadata["lengh_samples"] / audio_metadata["sample_rate"] / 60
+    audio_metadata["length_mins"] = int(audio_metadata["length_time"])
+    audio_metadata["length_seconds_remainder"] = int((audio_metadata["length_time"] - audio_metadata["length_mins"]) * 60)
+    song_file.close()
+    print_audio_metadata(audio_metadata)
+
+def decode_24bit_to_int32(raw_bytes):
+    # This is a placeholder for now
+    # TODO: implement this for 24 bit audio 
+    raise NotImplementedError("24 bit audio not supported yet")
+
+def convert_bytes_to_numpy_array(raw_bytes, sample_width):
+    # convert raw WAV framse to a NumPy array based on given sample width
+    match sample_width:
+        case 1:
+            samples = np.frombuffer(raw_bytes, dtype=np.uint8)
+        case 2:
+            samples = np.frombuffer(raw_bytes, dtype=np.int16)
+        case 3:
+            # insert long winded thing here
+            samples = decode_24bit_to_int32(raw_bytes)
+        case 4:
+            samples = np.frombuffer(raw_bytes, dtype=np.float32)
+        case _: # default
+            raise ValueError("Unsupported bit depth")
+
+    # print(f"Samples: {samples}")
+    return samples
+
+def read_wav_in_chunks(audio_path, chunk_size=1024, audio_metadata=None):
+    if audio_metadata is None:
+        raise Exception("Unknown error, audio metadata is missing")
+    if type(audio_metadata) is not dict:
+        raise Exception("Unknown error, audio metadata is not a dictionary")
+    print(f"metadata type: {type(audio_metadata)}")
+    with wave.open(audio_path, "rb") as song_file:
+        # i = 0
+        # i for debugging purposes
+        while True:
+            raw_bytes = song_file.readframes(chunk_size)
+            if not raw_bytes:
+                break
+            samples = convert_bytes_to_numpy_array(raw_bytes, audio_metadata["sample_width"])
+
+            if audio_metadata["num_channels"] > 1:
+                samples = samples.reshape(-1, audio_metadata["num_channels"])
+
+            # print(f"Chunk {i}: {samples}")
+            # i += 1
+            yield samples
+
+def get_audio_chunks(audio_path, chunk_size=1024, audio_metadata=None):
+    chunks = []
+    for i, chunk in enumerate(read_wav_in_chunks(audio_path, chunk_size, audio_metadata)):
+        print(f"Got chunk {i}, shape: {chunk.shape}")
+        chunks.append(chunk)
+    return chunks
