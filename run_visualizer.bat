184a9c51dc414926f558b102d744e34f00f336dc
Kindrid <151909224+kindiregg@users.noreply.github.com>
2025-02-11 18:01:28 -0600

    Make run_visualizer.bat actually work

--- run_visualizer.bat
+++ run_visualizer.bat
@@ -1,1 +1,17 @@
-py .\main.py\
+@echo off
+set VENV_DIR=venv
+
+if not exist %VENV_DIR% (
+    echo Creating virtual environment...
+    python -m venv %VENV_DIR%
+)
+
+call %VENV_DIR%\Scripts\activate
+
+echo Installing dependencies...
+pip install -r requirements.txt
+
+echo Running main.py...
+python main.py
+
+pause
